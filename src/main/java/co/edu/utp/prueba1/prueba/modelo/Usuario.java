9b8f8d89e18b7c090d31d8794c59296ffb85d97f
juanjo <juanjose.granadatp.edu.co>
2022-06-01 09:32:06 -0500

    Clase Día 2

--- src/main/java/co/edu/utp/prueba1/prueba/modelo/Usuario.java
+++ src/main/java/co/edu/utp/prueba1/prueba/modelo/Usuario.java
@@ -1,10 +1,12 @@
 package co.edu.utp.prueba1.prueba.modelo;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @AllArgsConstructor
 @Data
+@Builder
 public class Usuario {
     private int id;
     private String nombre;
--- src/main/java/co/edu/utp/prueba1/prueba/servicios/UsuarioServicio.java
+++ src/main/java/co/edu/utp/prueba1/prueba/servicios/UsuarioServicio.java
@@ -2,9 +2,22 @@ package co.edu.utp.prueba1.prueba.servicios;
 
 import co.edu.utp.prueba1.prueba.modelo.Usuario;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UsuarioServicio {
 
+    private List<Usuario> usuarios = new ArrayList<>();
+
     public Usuario guardarUsuario(Usuario usuario){
-        return null;
+        if(usuarios.isEmpty()){
+            usuario.setId(0);
+        } else{
+            usuario.setId(usuarios.size()); //ID del ultimo usuario
+        }
+        usuarios.add(usuario);
+        Usuario usuarioResponse = Usuario.builder().id(usuario.getId()).build();
+        
+        return usuarioResponse;
     }
 }
