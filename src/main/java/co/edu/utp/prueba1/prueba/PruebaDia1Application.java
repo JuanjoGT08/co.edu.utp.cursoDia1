85bd22bf2fee1b361e3f8ff4ff7b261576913802
juanjo <juanjose.granadatp.edu.co>
2022-06-01 01:03:02 -0500

    se agregó un comentario

--- src/main/java/co/edu/utp/prueba1/prueba/PruebaDia1Application.java
+++ src/main/java/co/edu/utp/prueba1/prueba/PruebaDia1Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//Git 
 public class PruebaDia1Application {
 
 	public static void main(String[] args) {
