e91b7dd8d6b39788b94d28f7c781f8c2552a7f1b
juanjo <juanjose.granadatp.edu.co>
2022-06-01 00:52:42 -0500

    mi primer commit

---
+++ src/main/java/co/edu/utp/prueba1/prueba/PruebaDia1Application.java
@@ -0,0 +1,13 @@
+package co.edu.utp.prueba1.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaDia1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaDia1Application.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/utp/prueba1/prueba/PruebaDia1ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.utp.prueba1.prueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaDia1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
